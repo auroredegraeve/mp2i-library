a42a5c1f734b84c292c6dff114cbbcabf13c2ada
auroredegraeve <90353069+auroredegraeve@users.noreply.github.com>
2021-11-06 16:03:41 +0100

    Update tri.ml

--- ds/tri/tri.ml
+++ ds/tri/tri.ml
@@ -1,5 +1,5 @@
 (*tri fusion*)
-let rec fusion l1 l2 = match l1, l2 with
+let rec fusion l1 l2 = match l1, l2 with (*les listes sont déjà triées*)
     | [], _ -> l2
     | _, [] -> l1
     | e1::q1, e2::q2 -> if e1 < e2 then e1::fusion q1 l2
