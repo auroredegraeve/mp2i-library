509d89a73b985108eff62c6fcfa1ab9f66afaa7a
auroredegraeve <90353069+auroredegraeve@users.noreply.github.com>
2021-11-06 12:11:46 +0100

    Update tableau.ml

--- ds/tableau/tableau.ml
+++ ds/tableau/tableau.ml
@@ -60,16 +60,3 @@ let tranche_max t =
         m := max !m !m_cur
     done;
     !m;;
-
-
-(* rechercher par trichotomie *)
-let tricho t e =
-    let rec aux i j =
-        if i > j then false
-        else let m1 = (2*i + j + 1)/3 in
-            let m2 = (i + 2*j + 2)/3 in
-            if t.(m1) = e || t.(m2) = e then true
-            else if e < t.(m1) then aux i (m1 - 1)
-            else if e < t.(m2) then aux (m1 + 1) (m2 - 1)
-            else aux (m2 + 1) j in
-    aux 0 (Array.length t - 1);;
