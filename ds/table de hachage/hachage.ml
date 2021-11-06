ee0e2cd6c583c6c3fe353f2fddf2ebdbca8cb9a5
auroredegraeve <90353069+auroredegraeve@users.noreply.github.com>
2021-11-06 18:21:36 +0100

    Update hachage.ml

--- ds/table de hachage/hachage.ml
+++ ds/table de hachage/hachage.ml
@@ -18,3 +18,21 @@ let hashtable_get ht k =
 (*supprimer une clé *)
 let hashtable_del ht k =
     ht.t.(ht.h k) <- None;;
+
+
+type ('k, 'v) dict = {
+    add : 'k * 'v -> unit;
+    del : 'k -> unit;
+    get : 'k -> 'v option
+};;
+
+
+let dict_of_hashtable n =
+    let ht = {
+        t = Array.make n None;
+        h = fun k -> k mod n
+        } in {
+            add = hashtable_add ht;
+            get = hashtable_get ht;
+            del = hashtable_del ht
+    };;
